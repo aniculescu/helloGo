6b434044bccdb8ad1617382c85a522a0b81a3402
Andrew Niculescu <aniculescu@ncsoft.com>
2019-10-26 15:15:22 -0700

    slice work. and variadic functions

--- adder/adder.go
+++ adder/adder.go
@@ -7,10 +7,38 @@ func Adder(n1, n2 int) int {
 	return n1 + n2
 }
 
+//Sum Function
 func Sum(numbers []int) int {
 	result := 0
 	for _, number := range numbers {
 		result += number
 	}
 	return result
+}
+
+//SumAll Function
+//Takes 1 or more slices
+//Returns: a slice
+func SumAll(slices ...[]int) []int {
+	var totals []int
+	for _, slice := range slices {
+		totals = append(totals, Sum(slice))
+	}
+	return totals
+}
+
+//SumAllTrails Function
+func SumAllTrails(slices ...[]int) []int {
+	var totals []int
+	//var tail []int
+	mySum := 0
+	for _, slice := range slices {
+		if len(slice) > 0 {
+			mySum = Sum(slice[1:])
+		} else {
+			mySum = 0
+		}
+		totals = append(totals, mySum)
+	}
+	return totals
 }
--- adder/adder_test.go
+++ adder/adder_test.go
@@ -2,6 +2,7 @@ package integers
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -27,4 +28,37 @@ func TestSum(t *testing.T) {
 	if total != expected {
 		t.Errorf("Expected %d, received %d in %v", expected, total, numSlice)
 	}
+}
+func TestSumAll(t *testing.T) {
+	slice1 := []int{1, 2}
+	slice2 := []int{0, 9}
+	total := SumAll(slice1, slice2)
+	expected := []int{3, 9}
+	if !reflect.DeepEqual(total, expected) {
+		t.Errorf("Expected %v, received %v + %v", expected, slice1, slice2)
+	}
+}
+
+func TestSumAllTrails(t *testing.T) {
+	assertSum := func(t *testing.T, want, got []int) {
+		t.Helper()
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Expected %v, received %v", want, got)
+		}
+	}
+	t.Run("Adding slice trails", func(t *testing.T) {
+		slice1 := []int{2, 3}
+		slice2 := []int{0, 1, 1}
+		trailTotal := SumAllTrails(slice1, slice2)
+		expected := []int{3, 2}
+		assertSum(t, expected, trailTotal)
+	})
+
+	t.Run("Adding slice trail with empty slice", func(t *testing.T) {
+		slice1 := []int{}
+		slice2 := []int{0, 1, 1}
+		trailTotal := SumAllTrails(slice1, slice2)
+		expected := []int{0, 2}
+		assertSum(t, expected, trailTotal)
+	})
 }
